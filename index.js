456159ed7ef8caa94adb6fb2e6165ac241dc1b24
ed benedict quia <edbenedictquia@gmail.com>
2022-10-23 12:55:48 +0800

    update index.js

--- index.js
+++ index.js
@@ -14,14 +14,39 @@ function computerChoice(){
 
 //play a single round of rock paper scissors
 function oneround(){
+//get the choice from computer
     let computer = computerChoice();
+//get the choice from user
     let user = prompt("Please enter your choice: rock, paper or scissors");
     user = user.toLowerCase();
-    console.log(user);
+    console.log('user chose '+user);
+    console.log('computer chose '+computer);
+//return the winner of the round
+    if(computer=='rock' && user=='paper' || computer=='paper' && user=='scissors' || computer=='scissors' && user=='rock'){
+        return 'user won';
+    }
+    else if(computer == user){
+        return 'tie';
+    }
+    else{
+        return 'computer won';
+    }
 }
 
-//get the choice from computer
-//get the choice from user
-//return the winner of the round
-
 //play 5 rounds of rock paper scissors
+function game(){
+    for(let i=0;i<5;i++){
+        let game = oneround();
+        if(game=='user won'){
+            console.log('user won');
+        }
+        else if(game=='computer won'){
+            console.log('computer won');
+        }
+        else if(game=='tie'){
+            console.log('its a tie');
+        }
+    }
+}
+
+game();
